713b9a028012702ec17ed32f283e6174388dcd3b
Anderson <andemartinezdev@gmail.com>
2021-02-03 16:14:59 -0500

    4-14 -> One Way Data Binding
    - Se crearon metodos, los cuales cambian la data de variables, haciendo que se refresque el html.

--- 01-bases/src/app/heroes/heroe/heroe.component.ts
+++ 01-bases/src/app/heroes/heroe/heroe.component.ts
@@ -9,11 +9,20 @@ export class HeroeComponent {
     nombre: string = 'Iron Man';
     edad  : number = 45;
 
-    get nombreCapitalizado() {
+    get nombreCapitalizado(): string {
         return this.nombre.toUpperCase();
     }
 
     obtenerNombre(): string {
         return `${this.nombre} - ${this.edad}`;
+    }
+
+
+    cambiarNombre():void {
+        this.nombre = 'Spider Man';
+    }
+
+    cambiarEdad(): void {
+        this.edad = 30;
     }
 }
