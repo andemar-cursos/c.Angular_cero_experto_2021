b81424dafb9c3548f3275489d3597e152203c0b8
Anderson <andemartinezdev@gmail.com>
2021-02-03 16:35:47 -0500

    4-15 -> Create component auto
    - Se creo el componente de manera automarica con el ng g c ...../.....

---
+++ 01-bases/src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+
+}
