8dc3769f4dfd53a4c80821026dc5e2cb8485c014
Anderson <andemartinezdev@gmail.com>
2021-02-07 11:14:03 -0500

    5-7 -> ngModel
    - Se uso el ngModel para la transferencia de datos de los campos de un formulario.

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  // Obetos
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 14000,
+  };
+
   // Metodos
   agregar(): void {
-    console.log('Esto es una prueba');
+    console.log(this.nuevo);
   }
 }
