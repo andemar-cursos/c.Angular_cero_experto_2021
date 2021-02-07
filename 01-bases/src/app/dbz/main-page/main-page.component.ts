b50bbf1bfb736f8ffbb78b9386dab7d0b41085df
Anderson <andemartinezdev@gmail.com>
2021-02-07 13:55:55 -0500

    5-14 -> Dbz Service
    - Se creo el servicio de Dbz el cual contendra todos los metodos que manejan la data.

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+
+  constructor( private dbzService: DbzService) {}
+
   personajes: Personaje[] = [
     {
       nombre: 'Goku',
