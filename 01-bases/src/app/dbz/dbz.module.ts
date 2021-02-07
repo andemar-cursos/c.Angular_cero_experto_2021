ff183894206ee434df7a1fa5e5020dba5e5ecd3f
Anderson <andemartinezdev@gmail.com>
2021-02-07 11:03:55 -0500

    5-6 -> FormsModule
    - Se importo el FormsModule, para evitar el refresh de la pagina cuando se hace submit de un formulario.

--- 01-bases/src/app/dbz/dbz.module.ts
+++ 01-bases/src/app/dbz/dbz.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
-
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MainPageComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    MainPageComponent
-  ]
+  exports: [MainPageComponent],
+  imports: [CommonModule, FormsModule],
 })
-export class DbzModule { }
+export class DbzModule {}
--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class MainPageComponent {
+  // Metodos
+  agregar(): void {
+    console.log('Esto es una prueba');
   }
-
 }
