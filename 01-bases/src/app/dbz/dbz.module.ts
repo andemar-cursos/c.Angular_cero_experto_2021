d3adcca8b6f79f4dc2b717a3b7942ac44a379660
Anderson <andemartinezdev@gmail.com>
2021-02-07 11:56:54 -0500

    5-9 -> Child component
    - Se creo un componente hijo, el cual visualizara la lista de personajes.

--- 01-bases/src/app/dbz/dbz.module.ts
+++ 01-bases/src/app/dbz/dbz.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule } from '@angular/forms';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, PersonajesComponent],
   exports: [MainPageComponent],
   imports: [CommonModule, FormsModule],
 })
