2361769e2f3c35d2e3b864a53479884aafcff2a5
Anderson <andemartinezdev@gmail.com>
2021-02-14 14:49:31 -0500

    8-20 -> verPais Component
    - Se obtiene los parametros del URL, mediante el activateRoute.
    - Se creo el servicio byAlpha el cual obtiene el pais por su identificador.

--- 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  // Atributos
 
-  constructor() { }
+  // Constructor
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private paisSerice: PaisService
+  ) {}
 
   ngOnInit(): void {
+    this.activateRoute.params.subscribe(({ id }) => {
+      this.paisSerice.byAlpha(id).subscribe(console.log);
+    });
   }
 
+  // Metodos
 }
