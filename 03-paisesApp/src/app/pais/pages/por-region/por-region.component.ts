679ca4154c13aa0aa673473288c4198a22c1309e
Anderson <andemartinezdev@gmail.com>
2021-02-14 15:42:51 -0500

    9-6 -> PaisesRegion
    - Se creo el servicio y la lista que muestra los paises de una region.

--- 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interfaces';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -15,13 +17,25 @@ export class PorRegionComponent {
   // Atributos
   regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
   regionActiva: string = '';
+  paises: Country[] = [];
 
   // Constructor
-  constructor() {}
+  constructor(private paisService: PaisService) {}
 
   // Metodos
   activarRegion(region: string): void {
+    if (region === this.regionActiva) {
+      return;
+    }
+
     this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService
+      .buscarRegion(region)
+      .subscribe((paises) => (this.paises = paises));
+  }
+
   getClaseCss(region: string): string {
     return region === this.regionActiva
       ? 'btn btn-primary'
