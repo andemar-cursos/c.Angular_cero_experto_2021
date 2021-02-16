8a7cc23f14322d0edf228cf3de30e66333d0e205
Anderson <andemartinezdev@gmail.com>
2021-02-15 20:59:36 -0500

    10-10 -> MenuBar primeNg
    - Se creo el menuBar en menu.component.

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-// PrimeNg
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PrimeNgModule],
+  imports: [BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
