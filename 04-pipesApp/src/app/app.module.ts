639017b586328b36730bd2e042421cc2b783fb6b
Anderson <andemartinezdev@gmail.com>
2021-02-15 21:08:38 -0500

    10-11 -> Router
    - Se crearon las rutas de las paginas de ventas.

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, AppRouterModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
