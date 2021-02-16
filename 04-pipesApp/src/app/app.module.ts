10898412b32b0593d5a8935291037bbb94e0bc83
Anderson <andemartinezdev@gmail.com>
2021-02-15 21:26:37 -0500

    10-13 -> PrimeFlex
    - Se uso un p-grid el cual, su contenido seran los pipes a demostrar.
    - Se hizo la instalacion de primeFlex: npm install primeflex

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouterModule, SharedModule],
+  imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
   providers: [],
   bootstrap: [AppComponent],
 })
