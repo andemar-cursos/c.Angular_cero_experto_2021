a821761d7ca6905e213ea4df497447eac476cca1
Anderson <andemartinezdev@gmail.com>
2021-02-15 20:26:58 -0500

    10-7 -> PrimeButton
    - Se implementaron los estilos de primeng.

--- 04-pipesApp/src/app/app.component.ts
+++ 04-pipesApp/src/app/app.component.ts
@@ -3,12 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
   nombre: string = 'Andemar';
-
 
   mostrarNombre(): void {
     console.log(this.nombre);
