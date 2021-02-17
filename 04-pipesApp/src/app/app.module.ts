16ce5c912d3d6145199d5baf543494a222acfb09
Anderson <andemartinezdev@gmail.com>
2021-02-17 17:31:06 -0500

    10-16 -> Cambiar lenguaje
    - Se hizo al configuracion, para que el idioma "nativo" de la app sea es-CO.

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,10 +7,17 @@ import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
+// Cmabiar el locale de la app
+import localeEs from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-CO'}
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
