c398df89fa0b27529efacce636e9ff8fa433bdcf
Anderson <andemartinezdev@gmail.com>
2021-02-17 18:23:59 -0500

    10-22 -> I18nPluralPipe
    - Se uso el I18nPlural.

--- 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,12 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class NoComunesComponent {
-  // Atributos
+  // Atributos I18nSelect
   nombre: string = 'Andemar';
   genero: string = 'masculino';
 
   invitacionMap = {
     masculino: 'invitarlo',
     femenino: 'invitarla',
+  };
+
+  // Atributos I18Plural
+  clientes: string[] = ['a', 'n', 'd', 'e', 'm', 'a', 'r'];
+  clientesMap = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos dos clientes esperando',
+    other: 'tenemos # clientes esperando'
   };
 }
