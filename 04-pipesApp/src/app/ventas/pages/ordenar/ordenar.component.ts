1d42378912c42057ed59c2835cfa1f78629f453a
Anderson <andemartinezdev@gmail.com>
2021-02-18 15:29:14 -0500

    11-7 -> Data Table PrimeNg
    - Se ingresaron los datos de un array a la tabla del primeNg.

--- 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class OrdenarComponent {
   // Atributos
 
   enMayus: boolean = true;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Super man',
+      vuela: true,
+      color: Color.azul,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde,
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo,
+    },
+    {
+      nombre: 'Linterna verde',
+      vuela: true,
+      color: Color.verde,
+    },
+  ];
 }
