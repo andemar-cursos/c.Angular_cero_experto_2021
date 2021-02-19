baf48f6e39d7c6eed139b812b6d0fd2ae9e0b2f6
Anderson <andemartinezdev@gmail.com>
2021-02-18 21:05:38 -0500

    12-7 -> Rutas hijas y Lazyload
    - Se crearon rutas hijas del componentes de auth y se les implemento la carga perezosa.

--- 05-heroesApp/src/app/app-routing.module.ts
+++ 05-heroesApp/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
     path: '404',
     component: ErrorPageComponent,
   },
   {
     path: '**',
-    redirectTo: '404'
+    redirectTo: '404',
   },
 ];
 
