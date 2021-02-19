7bb24ee73dd084a68ef8d42b2437cdae40707b81
Anderson <andemartinezdev@gmail.com>
2021-02-18 19:48:55 -0500

    12-5 -> Modulos y componentes
    - Se crearon los modulos y componentes que se usaran.

--- 05-heroesApp/src/app/app.module.ts
+++ 05-heroesApp/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, ErrorPageComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
