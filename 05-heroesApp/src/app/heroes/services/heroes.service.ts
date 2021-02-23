344c51ad937f311732c5d1bd2dfe1438f01df3b1
Anderson <andemartinezdev@gmail.com>
2021-02-23 12:53:50 -0500

    13-17 -> Variables de entorno
    - Se creo una variable de entorno [baseUrl], que apuntara a la base del endpoint.

--- 05-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 05-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -2,24 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Heroe } from '../interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
   // Atributos
-  endpoint: string = 'http://localhost:3000/heroes';
+  endpoint: string = `${environment.baseUrl}/heroes`;
 
   // Constructor
   constructor(private http: HttpClient) {}
-  
 
   // Metodos
 
   getHeroes(): Observable<Heroe[]> {
     return this.http.get<Heroe[]>(this.endpoint);
   }
-  
+
   getById(id: string): Observable<Heroe> {
     return this.http.get<Heroe>(`${this.endpoint}/${id}`);
   }
