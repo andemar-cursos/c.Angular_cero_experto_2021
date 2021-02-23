503073ea1285f7203e7ba007f72ebc17ddc59ad5
Anderson <andemartinezdev@gmail.com>
2021-02-22 20:37:09 -0500

    13-8 -> Heroes Service
    - Se implemento el service de Heroes.

--- 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ListadoComponent implements OnInit {
-
-  constructor() { }
+  constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
+    this.heroesService.getHeroes().subscribe(console.log);
   }
-
 }
