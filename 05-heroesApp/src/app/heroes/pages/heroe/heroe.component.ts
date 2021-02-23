d4b91b0410ff3a8348af68e1c843065e440e24f2
Anderson <andemartinezdev@gmail.com>
2021-02-23 12:21:48 -0500

    13-14 -> Ruta Heroe
    - Se agregaron los routersLinks a los botones de las tarjetas.

--- 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
+++ 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeroeComponent implements OnInit {
+  // Atributos
+  heroeId: string = "";
 
-  constructor() { }
+  // Constructor
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.heroeId = this.activatedRoute.snapshot.params['id'];
+    console.log(this.heroeId);
   }
 
+  //Metodos
 }
