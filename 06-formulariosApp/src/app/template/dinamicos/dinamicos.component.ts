65af159489bceb98185750b1e7a75b7a1f8c936e
Anderson <andemartinezdev@gmail.com>
2021-02-27 15:30:24 -0500

    16-15 -> Dinamic element
    - Se implemento de forma dinamica, un input que sirve como lista de favoritos agregados.

--- 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
+++ 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [],
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
   // Atributos
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
+  persona: Persona = {
+    nombre: 'andemar',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'Clanad' },
+    ],
+  };
+
   // Cosntructor
   constructor() {}
-
-  ngOnInit(): void {}
 
   // Metodos
   guardar(): void {
