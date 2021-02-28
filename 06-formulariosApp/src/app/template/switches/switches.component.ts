4080fbe5e30f1235ba1cf966615147bdd77aba4d
Anderson <andemartinezdev@gmail.com>
2021-02-28 15:15:25 -0500

    16-19 -> Validate Radio, Check & Switch
    - Se aplicaron las validaciones de cada uno.

--- 06-formulariosApp/src/app/template/switches/switches.component.ts
+++ 06-formulariosApp/src/app/template/switches/switches.component.ts
@@ -3,14 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
+  // Atributos
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
+  terminosYCondiciones: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  // Metodos
 }
