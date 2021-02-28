14ef0b72d1a04dd56ec21a2f20231dcc54cbcb06
Anderson <andemartinezdev@gmail.com>
2021-02-28 16:27:37 -0500

    17-8 -> Submit
    - Se agrega un ngSubmit al formulario.
    - El submit ejecuta un metodo, el cual valida que no haya errores, 'guarda' y resetea el formulario.

--- 06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ 06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './basicos.component.html',
   styles: [],
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
   // Atributos
 
   // Manera manual
@@ -26,11 +26,32 @@ export class BasicosComponent {
   // Constructor
   constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'RTX 4090ti',
+      precio: 1500,
+      existencias: 4,
+    });
+  }
+
   // Metodos
   campoEsValido(campo: string): boolean | null {
     return (
       this.miFormulario.controls[campo].errors &&
       this.miFormulario.controls[campo].touched
     );
+  }
+
+  guardar(): void {
+    if (this.miFormulario.invalid) {
+      // Esto se ejecuta, para que los campos sean "toched"
+      // y no se cumpla el metodo campoEsValido, retornando
+      // los errores.
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    this.miFormulario.reset();
+    console.log(this.miFormulario.value);
   }
 }
