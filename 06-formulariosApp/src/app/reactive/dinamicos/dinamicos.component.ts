ba77bd56823cccaded9896fe2e356c9abcde0aa5
Anderson <andemartinezdev@gmail.com>
2021-03-01 20:44:44 -0500

    17-9 -> Valid Dinamico
    - Se hizo la validacion del campo nombre del formulario dinamico.

--- 06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ 06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  // Atributos
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+  });
 
-  constructor() { }
+  // Constructor
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  // Metodos
+  guardar(): void {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
   }
 
+  campoEsValido(campo: string): boolean | null {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
 }
