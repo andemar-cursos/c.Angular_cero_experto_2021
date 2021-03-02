db7f1910ee4f85d08e5eb15fc1d16dfbfb4a591a
Anderson <andemartinezdev@gmail.com>
2021-03-01 21:40:23 -0500

    17-13 -> Reactive Switches
    - Se establecieron los correspondientes atributos a los labels de html.

--- 06-formulariosApp/src/app/reactive/switches/switches.component.ts
+++ 06-formulariosApp/src/app/reactive/switches/switches.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesComponent implements OnInit {
+  // Atributos
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue],
+  });
 
-  constructor() { }
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
+
+  // Constructor
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    // Se agrega la condicion, ya que no viene este valor
+    // por defecto desde persona.
+    this.miFormulario.reset({ ...this.persona, condiciones: false });
   }
 
+  // Metodos
 }
