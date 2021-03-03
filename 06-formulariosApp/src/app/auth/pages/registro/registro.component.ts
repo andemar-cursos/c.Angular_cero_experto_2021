5a27338e3c9678fa8555598c3e382a97c1f28f28
Anderson <andemartinezdev@gmail.com>
2021-03-02 21:47:49 -0500

    18-7 -> Regex Email
    - Se implemento la exprecion regular, para el campo email.

--- 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -11,6 +11,7 @@ export class RegistroComponent implements OnInit {
 
   // Valida que el string sea: 'Nombre Apellido'
   nombreApellidoPattern: string = '([a-zA-Z+]+) ([a-zA-Z]+)';
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   // Atributos
   miFormulario: FormGroup = this.fb.group({
@@ -18,12 +19,18 @@ export class RegistroComponent implements OnInit {
       '',
       [Validators.required, Validators.pattern(this.nombreApellidoPattern)],
     ],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
   });
 
   // Constructor
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Andemar Martinez',
+      email: 'andemar@dev.com',
+    });
+  }
 
   // Metodos
   guardar(): void {
