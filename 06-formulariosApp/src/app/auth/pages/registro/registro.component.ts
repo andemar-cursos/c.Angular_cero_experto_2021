f841488ff2e24451b0bb5bf38c56c4e20fc72474
Anderson <andemartinezdev@gmail.com>
2021-03-02 21:42:20 -0500

    18-6 -> Regex
    - Se uso una exprecion regular, para la validacion del campo nombre.

--- 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -6,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class RegistroComponent implements OnInit {
-  constructor() {}
+  // TODO: Temporal
+
+  // Valida que el string sea: 'Nombre Apellido'
+  nombreApellidoPattern: string = '([a-zA-Z+]+) ([a-zA-Z]+)';
+
+  // Atributos
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [
+      '',
+      [Validators.required, Validators.pattern(this.nombreApellidoPattern)],
+    ],
+  });
+
+  // Constructor
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  // Metodos
+  guardar(): void {
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.markAllAsTouched();
+  }
+
+  campoNoValido(campo: string): boolean | null {
+    return (
+      this.miFormulario.controls[campo]?.errors &&
+      this.miFormulario.controls[campo]?.touched
+    );
+  }
 }
