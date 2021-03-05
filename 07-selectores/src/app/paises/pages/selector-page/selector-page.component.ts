24b9de347f7985069d8abeda94f06f3641adbd6b
Anderson <andemartinezdev@gmail.com>
2021-03-04 22:27:06 -0500

    19-8 -> Pais Selector
    - Se creo el selector, ingresando la data que trae el servicio de paises al seleccionar un contienente.

--- 07-selectores/src/app/paises/pages/selector-page/selector-page.component.ts
+++ 07-selectores/src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaisesService } from '../../services/paises.service';
+import { paisSmall } from '../../interfaces/paises.interface';
 
 @Component({
   selector: 'app-selector-page',
@@ -11,15 +12,24 @@ export class SelectorPageComponent implements OnInit {
   // Atributos
   miFormulario: FormGroup = this.fb.group({
     region: ['', [Validators.required]],
+    pais: ['', [Validators.required]],
   });
 
   regiones: string[] = [];
+  paises: paisSmall[] = [];
 
   // Constructor
   constructor(private fb: FormBuilder, private paisesService: PaisesService) {}
 
   ngOnInit(): void {
     this.regiones = this.paisesService.getRegiones;
+
+    // Cuando cambie la region
+    this.miFormulario.get('region')?.valueChanges.subscribe((region) => {
+      this.paisesService
+        .getPaisesByRegion(region)
+        .subscribe((paises) => (this.paises = paises));
+    });
   }
 
   // Metodos
--- 07-selectores/src/app/paises/services/paises.service.ts
+++ 07-selectores/src/app/paises/services/paises.service.ts
@@ -1,10 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { paisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaisesService {
   // Atributos
+  private baseUrl: string = 'https://restcountries.eu/rest/v2';
+
   private regiones: string[] = [
     'Africa',
     'Americas',
@@ -13,11 +18,16 @@ export class PaisesService {
     'Oceania',
   ];
 
-  // Constructor
-  constructor() {}
-
-  // Metodos
   get getRegiones(): string[] {
     return [...this.regiones];
+  }
+
+  // Constructor
+  constructor(private http: HttpClient) {}
+
+  // Metodos
+  getPaisesByRegion(region: string): Observable<paisSmall[]> {
+    const url = `${this.baseUrl}/region/${region}?fields=alpha3Code;name`;
+    return this.http.get<paisSmall[]>(url);
   }
 }
