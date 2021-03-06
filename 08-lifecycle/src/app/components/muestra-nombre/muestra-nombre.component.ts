133677521d485a9fa94a2bf8e37020aa84e51573
Anderson <andemartinezdev@gmail.com>
2021-03-06 15:41:47 -0500

    20-03 -> Init project
    Init project

---
+++ 08-lifecycle/src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-muestra-nombre',
+  templateUrl: './muestra-nombre.component.html',
+  styles: [],
+})
+export class MuestraNombreComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
