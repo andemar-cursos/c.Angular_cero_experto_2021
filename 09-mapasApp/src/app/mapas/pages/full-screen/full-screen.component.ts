a4c69ec7775f4fd3957c1079271ae75b5e0e669f
Anderson <andemartinezdev@gmail.com>
2021-03-06 18:28:29 -0500

    21-08 -> Move import, central & zoom.
    - Se movio la declaracion del mapa al OnInit el capp.component, para tenerlo de manera global.
    - Se agregaron como atributos, el punto centro del inicio del mapa y el nivel de zoom.

--- 09-mapasApp/src/app/mapas/pages/full-screen/full-screen.component.ts
+++ 09-mapasApp/src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from '../../../../environments/environment.prod';
 
 @Component({
   selector: 'app-full-screen',
@@ -18,10 +17,11 @@ export class FullScreenComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    (mapboxgl as any).accessToken = environment.mapboxToken;
     var map = new mapboxgl.Map({
       container: 'mapa',
       style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-76.3092861, 3.5407376],
+      zoom: 17,
     });
   }
 }
