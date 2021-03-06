932245b6132c80c178a9bcdd352caf3173ce8b3b
Anderson <andemartinezdev@gmail.com>
2021-03-06 18:41:03 -0500

    21-09 -> Diseno Zoom-range
    - Se creo el diseno del componente Zoom-range.

--- 09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ 09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        width: 100%;
+        height: 100%;
+      }
+
+      .row {
+        background-color: white;
+        border-radius: 5px;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        position: fixed;
+        z-index: 9999;
+      }
+    `,
+  ],
 })
 export class ZoomRangeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-76.3092861, 3.5407376],
+      zoom: 17,
+    });
   }
-
 }
