037ac1ffe8f28eed96fb877f59831e45adb915d1
Anderson <andemartinezdev@gmail.com>
2021-03-09 18:36:15 -0500

    22-11 -> Input barraComponent
    - Se agregaron inputs a barraComponent, para mostrar diferente info segun donde se necesite.

--- 10-graficasApp/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ 10-graficasApp/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
 
@@ -7,35 +7,44 @@ import { Label } from 'ng2-charts';
   templateUrl: './grafica-barra.component.html',
   styles: [],
 })
-export class GraficaBarraComponent {
+export class GraficaBarraComponent implements OnInit {
   // Atributos
+  @Input() horizontal: boolean = false;
+
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
-  public barChartLabels: Label[] = [
-    '2020',
-    '2021',
-    '2022',
-    '2023',
-    '2024',
-    '2025',
-    '2026',
+
+  @Input() barChartLabels: Label[] = [
+    // '2020',
+    // '2021',
+    // '2022',
+    // '2023',
+    // '2024',
+    // '2025',
+    // '2026',
   ];
+
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
-  public barChartData: ChartDataSets[] = [
-    {
-      data: [65, 59, 80, 81, 56, 55, 40],
-      label: 'Series A',
-    },
-    {
-      data: [28, 48, 40, 19, 86, 27, 90],
-      label: 'Series B',
-    },
+  @Input() barChartData: ChartDataSets[] = [
+      // {
+      //   data: [65, 59, 80, 81, 56, 55, 40],
+      //   label: 'Series A',
+      // },
+      // {
+      //   data: [28, 48, 40, 19, 86, 27, 90],
+      //   label: 'Series B',
+      // },
   ];
 
   // Constructor
+  constructor() {}
+
+  ngOnInit(): void {
+    this.horizontal ? (this.barChartType = 'horizontalBar') : null;
+  }
 
   // Metodos
 }
