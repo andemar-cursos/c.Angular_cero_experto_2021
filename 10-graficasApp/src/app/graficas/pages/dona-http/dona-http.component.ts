0b9699f2672874b1a267d719b7fb62741f2436d0
Anderson <andemartinezdev@gmail.com>
2021-03-09 19:02:32 -0500

    22-14 -> Dona-http Data1
    - Se manipulo la data de la peticion del http para ser mostrada en la dona.

--- 10-graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
+++ 10-graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -7,9 +9,31 @@ import { GraficasService } from '../../services/graficas.service';
   styles: [],
 })
 export class DonaHttpComponent implements OnInit {
+  // Atributos
+
+  // -- Doughnut
+  public doughnutChartLabels: Label[] = [];
+  public doughnutChartData: MultiDataSet = [];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colores: Color[] = [
+    {
+      backgroundColor: ['#0075ED', '#00BAF7', '#00E0DB', '#00F7AD', '#00ED63'],
+    },
+  ];
+
+  // Constructor
   constructor(private graficasService: GraficasService) {}
 
   ngOnInit(): void {
-    this.graficasService.getUsuariosRedesSociales().subscribe(console.log);
+    this.graficasService.getUsuariosRedesSociales().subscribe((data) => {
+      const labels = Object.keys(data);
+      const values = Object.values(data);
+
+      this.doughnutChartLabels = labels;
+      this.doughnutChartData.push(values);
+    });
   }
+
+  // Metodos
 }
