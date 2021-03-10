c2223fd20c3882bd646fe488b355b8bcee58e894
Anderson <andemartinezdev@gmail.com>
2021-03-09 19:08:03 -0500

    22-15 -> Dona-http Data2 RXJS
    - Se utilizaron operadores del rxjs para obtener la data de la peticion, como se necesita a la hora de implementarla en la dona.

--- 10-graficasApp/src/app/graficas/services/graficas.service.ts
+++ 10-graficasApp/src/app/graficas/services/graficas.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,5 +11,15 @@ export class GraficasService {
   // Metodo
   getUsuariosRedesSociales() {
     return this.http.get('http://localhost:3000/grafica');
+  }
+
+  getUsuariosRedesSocialesDonaData() {
+    return this.getUsuariosRedesSociales().pipe(
+      map((data) => {
+        const labels = Object.keys(data);
+        const values = Object.values(data);
+        return { labels, values };
+      })
+    );
   }
 }
