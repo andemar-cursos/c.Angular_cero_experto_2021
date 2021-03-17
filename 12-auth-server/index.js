2988e8e106834dfb77e55de8dd40f4f0321034fc
Anderson <andemartinezdev@gmail.com>
2021-03-16 21:19:04 -0500

    24-07 -> Conf express
    - Se hizo la configuracion principal en el servidor.

--- 12-auth-server/index.js
+++ 12-auth-server/index.js
@@ -1,1 +1,18 @@
-console.log('NMMS O.O');
+const express = require('express');
+
+
+// Crear el servidor/app de express
+const app = express();
+
+// GET
+app.get('/', ( req, res) => {
+    console.log(`Peticion en el /`)
+    res.status(500).json({
+        ok: true,
+        msg: 'todo Ok'
+    })
+})
+
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${4000}`);
+} )
