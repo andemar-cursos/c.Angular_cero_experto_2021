091df5518771f61bd86a116740287addfa7eec8b
Anderson <andemartinezdev@gmail.com>
2021-03-17 21:11:14 -0500

    24-13 -> Middleware
    - Se hace un "filtro" antes de ejecutar el endpoint el cual verifica los datos enviados en el body de la peticion.

--- 12-auth-server/routes/auth.js
+++ 12-auth-server/routes/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express');
+const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
 const router = Router();
@@ -8,7 +9,10 @@ router.post('/new', crearUsuario);
 
 
 // Login usuario
-router.post('/', loginUsuario);
+router.post('/', [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password es obligatorio').isLength({min: 6}),
+] ,loginUsuario);
 
 
 // Validar y revalidar token
